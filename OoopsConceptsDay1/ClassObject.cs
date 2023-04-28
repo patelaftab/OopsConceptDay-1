5f2fb26cb73dad146a2cd8429089a71572c98612
patelaftab <10patelaftab@gmail.com>
2023-04-28 12:28:30 +0530

    Creating Class And Object

---
+++ OoopsConceptsDay1/ClassObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoopsConceptsDay1
+{
+    public class ClassObject
+    {
+        public void CreatingClass()
+        {
+            int a = 10, b = 20, c;
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ OoopsConceptsDay1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoopsConceptsDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day-1 Oops Concepts");
+            Console.WriteLine("================================");
+            bool flag = true;
+            while (true)
+            {
+                Console.WriteLine("\nChoose a option From Below \n" +
+                    "Enter 1: Object Class Concept\n"
+                    );
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ClassObject classObject = new ClassObject();
+                        classObject.CreatingClass();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a Valid Option");
+                        break;
+                }
+            }
+        }
+    }
+}
