414a51188213aecded65d62822dfc24407a0ab3c
patelaftab <10patelaftab@gmail.com>
2023-05-02 22:31:35 +0530

    Encapsulation Concept With Properties

--- OoopsConceptsDay1/Program.cs
+++ OoopsConceptsDay1/Program.cs
@@ -16,8 +16,9 @@ namespace OoopsConceptsDay1
             while (true)
             {
                 Console.WriteLine("\nChoose a option From Below \n" +
-                    "Enter 1: Object Class Concept\n"+
-                    "Enter 2: Inheritance Concept\n"+"");
+                    "Enter 1: Object Class Concept\n" +
+                    "Enter 2: Inheritance Concept\n" + 
+                    "Enter 3: Encapsulation Concept\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -27,9 +28,16 @@ namespace OoopsConceptsDay1
                         break;
                     case 2:
                         Finding concept = new Finding();
-                        concept.AchivingInheritance("Aftab","Bio");
+                        concept.AchivingInheritance("Aftab", "Bio");
                         concept.AchInheritance("Patel");
                         concept.Information("Pune");
+                        break;
+                    case 3:
+                        Encapsulation encapsulation = new Encapsulation();
+                        encapsulation.Name = "Aftab";
+                        encapsulation.Address = "pune";
+                        encapsulation.RollNumber = 23;
+                        Console.WriteLine("Name is: "+encapsulation.Name+ "Address is: "+encapsulation.Address+ "Roll Number is: "+encapsulation.RollNumber);
                         break;
                     default:
                         Console.WriteLine("Choose a Valid Option");
